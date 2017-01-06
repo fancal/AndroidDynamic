aebace94477db2cb9e444c46038a0021fa686411
xfilshy <xfilshy@gmail.com>
2017-01-06 13:47:01 +0800

    ads

--- RF/LianShangCodeReader/app_load/src/main/java/com/elianshang/wms/app/load/provider/ContainerInfoProvier.java
+++ RF/LianShangCodeReader/app_load/src/main/java/com/elianshang/wms/app/load/provider/ContainerInfoProvier.java
@@ -20,7 +20,7 @@ import java.util.List;
  */
 public class ContainerInfoProvier {
 
-    private static final String base_url = "http://static.qatest.rf.lsh123.com/api/wms/rf/v1";
+    private static final String base_url = "http://static.qatest2.rf.lsh123.com/api/wms/rf/v1";
 
     /**
      * app唯一标示传imei
