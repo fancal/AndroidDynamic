9daecc3fa6ed074a5783a54b6e741e029c60014e
xfilshy <xfilshy@gmail.com>
2017-09-25 19:54:15 +0800

    iidasdads g`

--- RF/LianShangCodeReader/bridge_lib/src/main/java/com/elianshang/bridge/tool/HostTool.java
+++ RF/LianShangCodeReader/bridge_lib/src/main/java/com/elianshang/bridge/tool/HostTool.java
@@ -8,7 +8,8 @@ public class HostTool {
             new HostElement("qatest", "http://static.qatest.rf.lsh123.com/api/wms/rf/v1"),
             new HostElement("杭州01", "http://hd01.rf.wms.lsh123.wumart.com/api/wms/rf/v1"),
             new HostElement("test", "http://test.api.wms.lsh123.com/api/wms/rf/v1"),
-            new HostElement("static" , "http://static.rf.lsh123.com/api/wms/rf/v1")
+            new HostElement("static" , "http://static.rf.lsh123.com/api/wms/rf/v1"),
+            new HostElement("金盏" , "http://bj01.rf.wms.lsh123.wumart.com/api/wms/rf/v1")
     };
 
     public static HostElement curHost = hosts[0];
